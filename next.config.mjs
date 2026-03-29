1484dc96ab6e61b4fa8143d204f65d1ed5243805
LinkOne Developer <developer@linkwave.one>
2026-03-29 19:46:17 +0800

    Disable ESLint and TypeScript checks during build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // 在构建时禁用 ESLint 检查
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // 在构建时禁用 TypeScript 类型检查
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
